bf66c39ba8321cab80ffb0b1e3585588c4ba8c98
hemasanjusha <136702847+hemasanjusha@users.noreply.github.com>
2025-04-06 15:43:00 +0530

    Update app5.py

--- app5.py
+++ app5.py
@@ -14,7 +14,8 @@ english_words = set(nltk_words.words())
 
 # Streamlit page config
 st.set_page_config(page_title="📊 Student Feedback Sentiment Analysis", layout="wide")
-model = pipeline("sentiment-analysis", model="Hemasanjusha/student-feedback-sentiment-model")
+model = pipeline("sentiment-analysis", model="nlptown/bert-base-multilingual-uncased-sentiment")
+
 
 
 # Gibberish checker
